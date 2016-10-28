f407ed75e16934cc9fdef4a059ff24dd5f5d6a15
markedhero <markedhero@projectstudiogames.com>
2016-10-27 21:46:46 -0400

    first

---
+++ ObjectPool.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPool : MonoBehaviour {
+
+	PooledObject prefab;
+
+	List<PooledObject> availableObject = new List<PooledObject> ();
+
+	public PooledObject GetObject() {
+		PooledObject obj;
+		int lastAvailableIndex = availableObject.Count - 1;
+		if (lastAvailableIndex >= 0) {
+			obj = availableObject [lastAvailableIndex];
+			availableObject.RemoveAt (lastAvailableIndex);
+			obj.gameObject.SetActive (true);
+		} else {
+			obj = Instantiate<PooledObject> (prefab);
+			obj.transform.SetParent (transform, false);
+			obj.objectPool = this;
+		}
+
+		return obj;
+
+	}
+	public void AddObject( PooledObject o ) {
+		o.gameObject.SetActive (false);
+		o.gameObject.SetParent(transform,false);
+		availableObject.Add (o);
+	}
+
+	public static ObjectPool GetPool( PooledObject prefab ) {
+		GameObject obj;
+		ObjectPool pool; 
+		if (Application.isEditor) {
+			obj = GameObject.Find (prefab.name + " Pool");
+			if (obj) {
+				pool = obj.GetComponent<ObjectPool> ();
+				if (pool) {
+					return pool;
+				}
+			}
+		}
+		obj = new GameObject (prefab.name + " Pool");
+		pool = obj.AddComponent<ObjectPool> ();
+		pool.prefab = prefab;
+		return pool;
+	}
+
+	public void CreateObject() {
+		PooledObject obj = Instantiate<PooledObject>(prefab);
+		obj.transform.SetParent(transform,false);
+		obj.objectPool = this;
+		AddObject (obj);
+	}
+}
---
+++ PooledObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PooledObject : MonoBehaviour {
+
+	[System.NonSerialized]
+	ObjectPool poolInstanceForPrefab;
+
+	public ObjectPool objectPool {get; set;}
+
+	public void ReturnToPool() {
+		if (objectPool) {
+			objectPool.AddObject (this);
+		} else
+			Destroy (gameObject);
+	}
+
+	public T GetPooledInstance<T> () where T : PooledObject {
+		if (!poolInstanceForPrefab) {
+			poolInstanceForPrefab = ObjectPool.GetPool (this);
+		}
+		return (T)poolInstanceForPrefab.GetObject ();
+	}
+
+	public void FillPool<T> (int amount) where T : PooledObject {
+		if (!poolInstanceForPrefab) {
+			poolInstanceForPrefab = ObjectPool.GetPool (this);
+		}
+		for(int i = 0; i < amount; i++ ) 
+			poolInstanceForPrefab.CreateObject ();
+	}
+
+	public virtual void poolStart() {
+
+	}
+}
